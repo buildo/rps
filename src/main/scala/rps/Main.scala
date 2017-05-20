1d1e633186f3574073609e16d925c62fe91d95c2
Gabriele Petronella <gabriele@buildo.io>
2017-05-20 12:50:27 +0200

    Step 3

--- src/main/scala/rps/Main.scala
+++ src/main/scala/rps/Main.scala
@@ -25,32 +25,45 @@ trait Game {
   def play(): Unit = {
     import Result._
     val move = readLine("your move> ")
-    val (result, userMove, computerMove) = run(move)
-    result match {
-      case Win => println("You win!")
-      case Lose => println("You lose!")
-      case Draw => println("It's a draw!")
+    if (move == "q") {
+      println("Bye!")
+      sys.exit(0)
     }
-    println(s"User:     $userMove")
-    println(s"Computer: $computerMove")
+    run(move) match {
+      case None =>
+        println("Invalid move. Try again")
+        play()
+
+      case Some((result, userMove, computerMove)) =>
+        result match {
+          case Win => println("You win!")
+          case Lose => println("You lose!")
+          case Draw => println("It's a draw!")
+        }
+        println(s"User:     $userMove")
+        println(s"Computer: $computerMove")
+    }
   }
 
-  private def run(move: String): (Result, Move, Move) = {
+  private def run(move: String): Option[(Result, Move, Move)] = {
     val computerMove = generateComputerMove
     val userMove = toMove(move)
     import Move._
-    val result = (userMove, computerMove) match {
-      case (Rock, Scissor) | (Paper, Rock) | (Scissor, Paper) => Result.Win
-      case (m1, m2) if m1 == m2 => Result.Draw
-      case _ => Result.Lose
+    userMove.map { userMove =>
+      val result = (userMove, computerMove) match {
+        case (Rock, Scissor) | (Paper, Rock) | (Scissor, Paper) => Result.Win
+        case (m1, m2) if m1 == m2 => Result.Draw
+        case _ => Result.Lose
+      }
+      (result, userMove, computerMove)
     }
-    (result, userMove, computerMove)
   }
 
-  private def toMove(s: String): Move = s match {
-    case "1" => Move.Rock
-    case "2" => Move.Paper
-    case "3" => Move.Scissor
+  private def toMove(s: String): Option[Move] = s match {
+    case "1" => Some(Move.Rock)
+    case "2" => Some(Move.Paper)
+    case "3" => Some(Move.Scissor)
+    case _ => None
   }
 
   private def generateComputerMove: Move = {
