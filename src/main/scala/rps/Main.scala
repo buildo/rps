37eb20dee45db8738d9d724ef52603785da75f6c
Gabriele Petronella <gabriele@buildo.io>
2017-05-20 12:22:27 +0200

    WIP

--- src/main/scala/rps/Main.scala
+++ src/main/scala/rps/Main.scala
@@ -4,8 +4,10 @@ import scala.io.StdIn.readLine
 import scala.util.Random.shuffle
 import io.buildo.enumero.annotations.enum
 
-object Main extends App with Game {
-  play()
+import GameInstances._
+
+object Main extends App with Application {
+  play[GameKind.RPS.type]
 }
 
 @enum trait GameKind {
@@ -25,39 +27,51 @@ object Main extends App with Game {
   object Scissor
 }
 
-trait Game {
-  def play(): Unit = {
+trait Application {
+
+  def play[A <: GameKind](implicit gameRunner: GameRunner[A]): Unit = {
     val move = readLine("your move> ")
-    run(move).map {
+    gameRunner.run(move).map {
       case Result.Win => println("you win!")
       case Result.Lose => println("you lose!")
       case Result.Draw => println("it's a draw!")
     }.getOrElse {
       println(s"$move is an invalid move. Try again!")
-      play()
+      play[A]
     }
   }
 
-  private def run(move: String): Option[Result] = {
-    toMove(move).map { userMove =>
-      (generateComputerMove(), userMove) match {
-        case (m1, m2) if m1 == m2 => Result.Draw
-        case (Move.Rock, Move.Scissor) => Result.Win
-        case (Move.Scissor, Move.Paper) => Result.Win
-        case (Move.Paper, Move.Rock) => Result.Win
-        case _ => Result.Lose
+}
+
+trait GameRunner[A <: GameKind] {
+  def run(move: String): Option[Result]
+}
+
+object GameInstances {
+
+  implicit val rpsRunner: GameRunner[GameKind.RPS.type] = new GameRunner[GameKind.RPS.type] {
+
+    override def run(move: String): Option[Result] = {
+      toMove(move).map { userMove =>
+        (generateComputerMove(), userMove) match {
+          case (m1, m2) if m1 == m2 => Result.Draw
+          case (Move.Rock, Move.Scissor) => Result.Win
+          case (Move.Scissor, Move.Paper) => Result.Win
+          case (Move.Paper, Move.Rock) => Result.Win
+          case _ => Result.Lose
+        }
       }
     }
+
+    def toMove(move: String): Option[Move] = move match {
+      case "1" => Some(Move.Rock)
+      case "2" => Some(Move.Paper)
+      case "3" => Some(Move.Scissor)
+      case _   => None
+    }
+
+    def generateComputerMove(): Move =
+      shuffle(List(Move.Rock, Move.Paper, Move.Scissor)).head
+
   }
-
-  def toMove(move: String): Option[Move] = move match {
-    case "1" => Some(Move.Rock)
-    case "2" => Some(Move.Paper)
-    case "3" => Some(Move.Scissor)
-    case _   => None
-  }
-
-  def generateComputerMove(): Move =
-    shuffle(List(Move.Rock, Move.Paper, Move.Scissor)).head
-
 }
