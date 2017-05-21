5c83471258486fb65e429ab9236b70f2d1e5e4ba
Gabriele Petronella <gabriele@buildo.io>
2017-05-21 20:06:39 +0200

    Generic Game

--- src/main/scala/rps/Main.scala
+++ src/main/scala/rps/Main.scala
@@ -3,16 +3,10 @@ package rps
 import scala.io.StdIn.readLine
 import scala.util.Random.shuffle
 import io.buildo.enumero.annotations.enum
-import io.buildo.enumero.CaseEnumSerialization
+import io.buildo.enumero.{ CaseEnum, CaseEnumSerialization }
 
-object Main extends App with Game {
-  play()
-}
-
-@enum trait Move {
-  object Rock
-  object Paper
-  object Scissor
+object Main extends App with Player {
+  play(RPS)
 }
 
 @enum trait Result {
@@ -22,30 +16,32 @@ object Main extends App with Game {
 }
 
 trait Game {
-  def play(): Unit = {
-    import Result._
-    val move = readLine("your move> ")
-    if (move == "q") {
-      println("Bye!")
-      sys.exit(0)
-    }
-    run(move) match {
-      case None =>
-        println("Invalid move. Try again")
-        play()
+  type Move <: CaseEnum
+  def run(move: String): Option[(Result, Move, Move)]
+  def generateComputerMove(implicit ces: CaseEnumSerialization[Move]): Move =
+    shuffle(ces.values.toList).head
+}
 
-      case Some((result, userMove, computerMove)) =>
-        result match {
-          case Win => println("You win!")
-          case Lose => println("You lose!")
-          case Draw => println("It's a draw!")
-        }
-        println(s"User:     $userMove")
-        println(s"Computer: $computerMove")
+object RPS extends Game {
+  type Move = RPSMove
+
+  @enum trait RPSMove {
+    object Rock
+    object Paper
+    object Scissor
+  }
+
+  import RPSMove._
+
+  implicit class OrderedMove(m1: Move) extends Ordered[Move] {
+    def compare(m2: Move): Int = (m1, m2) match {
+      case (Rock, Scissor) | (Paper, Rock) | (Scissor, Paper) => 1
+      case (m1, m2) if m1 == m2 => 0
+      case _ => -1
     }
   }
 
-  private def run(move: String): Option[(Result, Move, Move)] = {
+  def run(move: String): Option[(Result, Move, Move)] = {
     val computerMove = generateComputerMove
     val userMove = toMove(move)
     userMove.map { userMove =>
@@ -58,23 +54,37 @@ trait Game {
     }
   }
 
-  implicit class OrderedMove(m1: Move) extends Ordered[Move] {
-    import Move._
-    def compare(m2: Move): Int = (m1, m2) match {
-      case (Rock, Scissor) | (Paper, Rock) | (Scissor, Paper) => 1
-      case (m1, m2) if m1 == m2 => 0
-      case _ => -1
-    }
-  }
-
   private def toMove(s: String): Option[Move] = s match {
-    case "1" => Some(Move.Rock)
-    case "2" => Some(Move.Paper)
-    case "3" => Some(Move.Scissor)
+    case "1" => Some(Rock)
+    case "2" => Some(Paper)
+    case "3" => Some(Scissor)
     case _ => None
   }
 
-  private def generateComputerMove: Move =
-    shuffle(CaseEnumSerialization[Move].values.toList).head
+}
+
+trait Player {
+  def play(game: Game): Unit = {
+    import Result._
+    val move = readLine("your move> ")
+    if (move == "q") {
+      println("Bye!")
+      sys.exit(0)
+    }
+    game.run(move) match {
+      case None =>
+        println("Invalid move. Try again")
+        play(game)
+
+      case Some((result, userMove, computerMove)) =>
+        result match {
+          case Win => println("You win!")
+          case Lose => println("You lose!")
+          case Draw => println("It's a draw!")
+        }
+        println(s"User:     $userMove")
+        println(s"Computer: $computerMove")
+    }
+  }
 
 }
