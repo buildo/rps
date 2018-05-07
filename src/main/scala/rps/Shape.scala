326aab258d0d50d218f4a5326674455a8cd01d3f
Cardelli Emanuele <cardu90@yahoo.it>
2018-05-07 21:27:58 +0200

    first step implementation

---
+++ src/main/scala/rps/Shape.scala
@@ -0,0 +1,25 @@
+package rps
+
+sealed trait Shape
+
+case object Rock extends Shape
+case object Paper extends Shape
+case object Scissor extends Shape
+
+//extractor for shape class
+object Shape {
+    def unapply(i: Int) : Option[Shape] = {
+        i match{
+            case 0 => Some(Rock)
+            case 1 => Some(Paper)
+            case 2 => Some(Scissor)
+            case _ => None
+        }
+    }   
+
+    def unapply(s: String) : Option[Shape] = try{
+        Shape.unapply(s.toInt)
+    } catch{
+        case _ => None
+    }
+}
