bf8247f96e669bacc8a12c0d26004b19f3a55857
Lorenzo Volpi <lorenzo.volpi@banksealer.com>
2019-07-04 12:34:51 +0200

    implementazione step 1

--- src/main/scala/rps/Main.scala
+++ src/main/scala/rps/Main.scala
@@ -1,5 +1,28 @@
 package rps
-
+import scala.io.StdIn.readLine
+import scala.util.Random
 object Main extends App {
-  println("ciao")
+  Game.play
+}
+
+object Game {
+  def play() : Unit = {
+    val moveMap = Map("0" -> "Rock", "1" -> "Paper", "2" -> "Scissor")
+    println("Choose Rock (0), Paper(1), Scissor(2)")
+    val move = moveMap.getOrElse(readLine(),"None")
+    val computerMove = moveMap(Random.nextInt(3).toString)
+    val resultString = s"$move VS $computerMove -> "
+    val result =(move, computerMove) match {
+      case (x,y) if x == y => resultString + "Draw!"
+      case ("Rock","Paper") => resultString  + "You lost!"
+      case ("Rock","Scissor") => resultString  + "You win!"
+      case ("Paper","Rock") => resultString  + "You win!"
+      case ("Paper","Scissor") => resultString  + "You lost!"
+      case ("Scissor","Rock") => resultString  + "You lost!"
+      case ("Scissor","Paper") => resultString  + "You win!"
+      case (_,_) =>  "Not an Option!"
+    }
+    println(result)
+    //play()
+  }
 }
