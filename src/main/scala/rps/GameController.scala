ead4002b2c9bc505ff94001c2aa52e564f77b6a4
Cardu <emanuele.cardelli@buildo.io>
2020-04-10 17:58:34 +0200

    adding annotations on GameController ZIO tasks definitions

--- src/main/scala/rps/GameController.scala
+++ src/main/scala/rps/GameController.scala
@@ -18,13 +18,13 @@ trait GameController {
 class GameControllerImpl(gameService: GameService)(implicit ec: ExecutionContext) extends GameController {
   val runtime = Runtime.default
 
-  private[this] def resultZIO = gameService
+  private[this] def resultZIO: URIO[Any, Either[Throwable, PlayResponse]] = gameService
     .getResult()
     .someOrFail(new IllegalStateException)
     .map(play => PlayResponse(play.userMove, play.computerMove, play.result))
     .either
 
-  private[this] def playZIO(userMove: Move) = gameService.playMove(userMove).either
+  private[this] def playZIO(userMove: Move): URIO[Any, Either[Throwable, UUID]] = gameService.playMove(userMove).either
 
   override def result(): Future[Either[Throwable, PlayResponse]] = runtime.unsafeRunToFuture(resultZIO)
 
