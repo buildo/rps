aa738747d13ea424a8d07dd97ea760b154032c63
Benedetto <bennesp@users.noreply.github.com>
2020-09-18 15:39:54 +0200

    fix styling issues

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -6,19 +6,16 @@ import rps.model.Result._
 import rps.model.{Move, Result}
 
 import scala.io.StdIn.readLine
+import scala.util.Random
 
 object Game {
   def play(): Unit = {
     val rawUserMove = readLine("your move (0: Rock, 1: Paper, 2: Scissors)> ")
     CaseEnumIndex[Move].caseFromIndex(rawUserMove) match {
-      case None =>
-        println("Sorry, you must enter a valid move (0, 1 or 2). Try again")
+      case None => println("Sorry, you must enter a valid move (0, 1 or 2). Try again")
       case Some(userMove) =>
         val computerMove = generateComputerMove()
-        println(
-          s"Your move: ${CaseEnumSerialization[Move].caseToString(userMove)}. Computer move: ${CaseEnumSerialization[Move]
-            .caseToString(computerMove)}"
-        )
+        println(s"Your move: ${CaseEnumSerialization[Move].caseToString(userMove)}. Computer move: ${CaseEnumSerialization[Move].caseToString(computerMove)}")
         val result = play(userMove, computerMove)
         result match {
           case Win  => println("You Win!")
--- src/test/scala/rps/GameTest.scala
+++ src/test/scala/rps/GameTest.scala
@@ -11,21 +11,21 @@ class GameTest extends AnyFlatSpec with should.Matchers {
   behavior of "Game"
 
   it should "win" in {
-    Game.play(Rock, Scissors) should be(Rock, Scissors, Win)
-    Game.play(Paper, Rock) should be(Paper, Rock, Win)
-    Game.play(Scissors, Paper) should be(Scissors, Paper, Win)
+    Game.play(Rock, Scissors) should be(Win)
+    Game.play(Paper, Rock) should be(Win)
+    Game.play(Scissors, Paper) should be(Win)
   }
 
   it should "lose" in {
-    Game.play(Rock, Paper) should be(Rock, Paper, Lose)
-    Game.play(Paper, Scissors) should be(Paper, Scissors, Lose)
-    Game.play(Scissors, Rock) should be(Scissors, Rock, Lose)
+    Game.play(Rock, Paper) should be(Lose)
+    Game.play(Paper, Scissors) should be(Lose)
+    Game.play(Scissors, Rock) should be(Lose)
   }
 
   it should "draw" in {
-    Game.play(Rock, Rock) should be(Rock, Rock, Draw)
-    Game.play(Paper, Paper) should be(Paper, Paper, Draw)
-    Game.play(Scissors, Scissors) should be(Scissors, Scissors, Draw)
+    Game.play(Rock, Rock) should be(Draw)
+    Game.play(Paper, Paper) should be(Draw)
+    Game.play(Scissors, Scissors) should be(Draw)
   }
 
 }
