ee318d90df69938d810ea84af04ddcad6f181f6d
Tommaso Petrucciani <tommaso.petrucciani@icloud.com>
2022-01-14 18:07:21 +0100

    Step 3

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -2,22 +2,30 @@ package rps
 
 import scala.io.StdIn
 import scala.util.Random
+import model.Move
+import Move._
 
 object Game {
   def play(): Unit = {
-    val userMove =
+    val rawUserMove =
       StdIn.readLine("Your move (0: Rock, 1: Paper, 2: Scissors)> ")
-    val computerMove = generateComputerMove()
-    println(s"Your move: $userMove. Computer move: $computerMove")
-    (userMove, computerMove) match {
-      case ("0", "2") | ("1", "0") | ("2", "1") => println("You win!")
-      case (x, y) if x == y                     => println("It's a draw!")
-      case _                                    => println("You lose :(")
+    Move.read(rawUserMove) match {
+      case None =>
+        println("Sorry, you must enter a valid move (0, 1 or 2). Try again")
+      case Some(userMove) =>
+        val computerMove = generateComputerMove()
+        println(
+          s"Your move: ${Move.show(userMove)}. Computer move: ${Move.show(computerMove)}"
+        )
+        (userMove, computerMove) match {
+          case (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) =>
+            println("You win!")
+          case (x, y) if x == y => println("It's a draw!")
+          case _                => println("You lose :(")
+        }
     }
   }
 
-  private val r = scala.util.Random
-
-  private def generateComputerMove(): String =
-    r.nextInt(3).toString
+  private def generateComputerMove(): Move =
+    Random.shuffle(List(Rock, Paper, Scissors)).head
 }
---
+++ src/main/scala/rps/model/Move.scala
@@ -0,0 +1,22 @@
+package rps.model
+
+sealed trait Move
+
+object Move {
+  case object Rock extends Move
+  case object Paper extends Move
+  case object Scissors extends Move
+
+  def read(s: String): Option[Move] = s match {
+    case "0" => Some(Rock)
+    case "1" => Some(Paper)
+    case "2" => Some(Scissors)
+    case _   => None
+  }
+
+  def show(m: Move) = m match {
+    case Rock     => "🗿"
+    case Paper    => "📄"
+    case Scissors => "✂️"
+  }
+}
