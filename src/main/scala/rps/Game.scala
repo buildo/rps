e2966bc70b286b881ed75e2e4fd5f08670c1509a
Tommaso Petrucciani <tommaso.petrucciani@icloud.com>
2024-03-08 11:30:03 +0100

    Step 2

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -8,16 +8,13 @@ object Game {
     val userMove = StdIn.readLine("Your move (0: Rock, 1: Paper, 2: Scissors)>")
     val computerMove = generateComputerMove()
     println(s"Your move: $userMove. Computer move: $computerMove")
-    if (userMove == computerMove) {
-      println("It's a draw!")
-    } else if (
-      userMove == "0" && computerMove == "2" ||
-      userMove == "1" && computerMove == "0" ||
-      userMove == "2" && computerMove == "1"
-    ) {
-      println("You win!")
-    } else {
-      println("You lose :(")
+    (userMove, computerMove) match {
+      case ("0", "2") | ("1", "0") | ("2", "1") =>
+        println("You win!")
+      case (x, y) if x == y =>
+        println("It's a draw!")
+      case _ =>
+        println("You lose :(")
     }
   }
 
